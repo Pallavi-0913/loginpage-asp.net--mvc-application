6521060542d09473303422d3bd9d8357546a4065
Pallavi Lenkalapelly <Pallavi Lenkalapelly@DESKTOP-54O0200>
2023-06-14 14:43:40 +0530

    Add project files.

---
+++ TMS/Models/salesLeadentity.cs
@@ -0,0 +1,13 @@
+﻿namespace TMS.Models
+{
+    public class salesLeadentity
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string email { get; set; }
+        public int phone { get; set; }
+        public string source { get; set; }
+    }
+}
---
+++ TMS/data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TMS.Models;
+
+namespace TMS.data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<salesLeadentity> SalesLead { get; set; }
+    }
+}
